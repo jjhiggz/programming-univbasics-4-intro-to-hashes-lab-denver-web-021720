7641771aaf692ec7033746ded01a77608a908539
Jonathan Higger <jonathan.higger@gmail.com>
2020-01-22 01:13:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,7 @@ def new_hash()
 end
 
 def my_hash()
-  return a = {'apples' :'Granny Smith' }
+  return a = {apples : 'Granny Smith' }
   # return a valid hash with any key/value pair of your choice
 end
 
